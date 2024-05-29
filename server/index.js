83a934e5a92e128d3ea8a9dc0c7dd62cc15e3f31
SorrenJ <soalexjao@gmail.com>
2024-05-29 04:13:19 -0700

    minimal index.js test

--- server/index.js
+++ server/index.js
@@ -1,41 +1,13 @@
-"use strict";
-
-// Basic express setup:
-
-const PORT = process.env.PORT || 5000;
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors"); // Add this line to import the CORS package
+const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-// Enable CORS for your client domain
-app.use(cors({ origin: 'https://tweeter-vercel-deploy1-client-89h5jniyx-sorrenjs-projects.vercel.app' }));
+app.use(express.static('public'));
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
-
-// The in-memory database of tweets. It's a basic object with an array in it.
-const db = require("./lib/in-memory-db");
-
-// The `data-helpers` module provides an interface to the database of tweets.
-const DataHelpers = require("./lib/data-helpers.js")(db);
-
-// Update the dates for the initial tweets (data-files/initial-tweets.json).
-require("./lib/date-adjust")();
-
-// The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
-// so it can define routes that use it to interact with the data layer.
-const tweetsRoutes = require("./routes/tweets")(DataHelpers);
-
-// Mount the tweets routes at the "/tweets" path prefix:
-app.use("/tweets", tweetsRoutes);
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
+app.get('/api', (req, res) => {
+  res.send({ message: 'API is working!' });
 });
 
 app.listen(PORT, () => {
-  console.log("Example app listening on port " + PORT);
+  console.log(`Server is running on port ${PORT}`);
 });
